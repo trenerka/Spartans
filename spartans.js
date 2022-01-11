7c1c8a193604f6d5c44210c28b755d686aca0012
trenerka <milan.pavlovic1998@gmail.com>
2022-01-11 14:48:42 +0100

    v2

--- spartans.js
+++ spartans.js
@@ -10,25 +10,24 @@ const saberi = (arg1, arg2) => {
 
 const memo = (saberi) => {
   return (...args) => {
-    const argsKey = args;
     //Ako je neki od argmenata objekat konvertuje se u niz
     if (
-      typeof argsKey[0] === "object" &&
-      !Array.isArray(argsKey[0]) &&
-      argsKey[0] !== null
+      typeof args[0] === "object" &&
+      !Array.isArray(args[0]) &&
+      args[0] !== null
     ) {
-      argsNiz[0] = Object.entries(argsKey[0]);
+      argsNiz[0] = Object.entries(args[0]);
     } else {
-      argsNiz[0] = argsKey[0];
+      argsNiz[0] = args[0];
     }
     if (
-      typeof argsKey[1] === "object" &&
-      !Array.isArray(argsKey[1]) &&
-      argsKey[1] !== null
+      typeof args[1] === "object" &&
+      !Array.isArray(args[1]) &&
+      args[1] !== null
     ) {
-      argsNiz[1] = Object.entries(argsKey[1]);
+      argsNiz[1] = Object.entries(args[1]);
     } else {
-      argsNiz[1] = argsKey[1];
+      argsNiz[1] = args[1];
     }
     //Ako je neki od argumenata niz sortira se
     if (Array.isArray(argsNiz[0])) {
@@ -37,7 +36,7 @@ const memo = (saberi) => {
     if (Array.isArray(argsNiz[1])) {
       argsNiz[1].sort();
     }
-    //Radi provera komutativnosti argumenata
+    //Radi provere komutativnosti argumenata
     const argsNiz1 = [argsNiz[1], argsNiz[0]];
     //Radi testiranja
     console.log("NIZ1", argsNiz);
@@ -57,12 +56,9 @@ const memo = (saberi) => {
 const newFunction = memo(saberi);
 
 // Testiranje
-console.time("First call");
+
 console.log(newFunction({ a: 3, b: 7, c: 5 }, [5, 5, 5]));
-console.timeEnd("First call");
-console.time("Second call");
+
 console.log(newFunction([9001], [23]));
-console.timeEnd("Second call");
-console.time("Third call");
+
 console.log(newFunction([5, 5, 5], { a: 3, c: 5, b: 7 }));
-console.timeEnd("Third call");
